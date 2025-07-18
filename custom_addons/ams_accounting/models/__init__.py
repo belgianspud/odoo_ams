ed08f3f8107835b2288115792e49fcff86a925e3
belgianspud <belgianspud@gmail.com>
2025-07-18 16:05:30 -0400

    base files for ams accounting module

---
+++ custom_addons/ams_accounting/models/__init__.py
@@ -0,0 +1,22 @@
+# Core accounting models (adapted from base_accounting_kit)
+from . import account_account
+from . import account_asset
+from . import account_followup
+from . import account_journal
+from . import account_move
+from . import account_payment
+from . import account_dashboard
+from . import credit_limit
+from . import product_template
+from . import res_config_settings
+from . import res_partner
+from . import res_company
+from . import multiple_invoice
+from . import multiple_invoice_layout
+from . import recurring_payments
+
+# AMS Integration models
+from . import ams_subscription_accounting
+from . import ams_member_financial
+from . import ams_chapter_financial
+from . import ams_financial_dashboard
---
+++ custom_addons/ams_accounting/wizard/__init__.py
@@ -0,0 +1,26 @@
+# Financial Report Wizards
+from . import financial_report
+from . import general_ledger
+from . import partner_ledger
+from . import tax_report
+from . import trial_balance
+from . import aged_partner
+from . import journal_audit
+from . import cash_flow_report
+
+# Book Report Wizards
+from . import account_bank_book_wizard
+from . import account_cash_book_wizard
+from . import account_day_book_wizard
+
+# Asset Management Wizards
+from . import asset_depreciation_confirmation_wizard
+from . import asset_modify
+
+# Administrative Wizards
+from . import account_lock_date
+
+# AMS Integration Wizards
+from . import ams_financial_report_wizard
+from . import ams_subscription_revenue_wizard
+from . import ams_member_financial_wizard
