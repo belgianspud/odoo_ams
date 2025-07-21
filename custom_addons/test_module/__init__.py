1261127ff8d6a975957e8ed4b2a87d11856fe6ea
belgianspud <belgianspud@gmail.com>
2025-07-21 16:29:37 -0400

    update for accounting module

--- custom_addons/test_module/__init__.py
+++
@@ -1,1 +0,0 @@
-print("=== TEST MODULE FOUND AND LOADING ===")
--- custom_addons/test_module/__manifest__.py
+++
@@ -1,7 +0,0 @@
-{
-    'name': 'Test Module',
-    'version': '18.0.1.0.0',
-    'depends': ['base'],
-    'installable': True,
-    'application': True,
-}
