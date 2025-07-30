2359546a6df258418882fd969f5243acc96c7686
belgianspud <belgianspud@gmail.com>
2025-07-30 15:49:21 -0400

    additional work on subscriptions module

--- custom_addons/ams_subscriptions/models/__init__.py
+++ custom_addons/ams_subscriptions/models/__init__.py
@@ -1,38 +1,10 @@
-# AMS Subscriptions - Model Imports
-# This file imports all the models for the AMS subscription system
-
-# Core subscription models
-from . import subscription
-from . import subscription_type
-from . import subscription_rule
-from . import subscription_status_history
-from . import subscription_renewal
-
-# Chapter management
+from . import membership_type
+from . import member_subscription
+from . import res_partner
 from . import chapter
-
-# Product integration
-from . import product
-from . import product_subscription_mixin
-
-# Partner/member integration
-from . import partner
-
-# Sales integration
+from . import subscription_benefit
+from . import subscription_stage
 from . import sale_order
 from . import account_move
-
-# Cron and automation
-from . import subscription_cron
-
-# Configuration and settings
-from . import res_config_settings
-
-# Financial integration
-from . import financial_transaction
-
-# Reporting models
-#from . import subscription_analytics
-
-# Wizard models (if any)
-# from . import wizards
+from . import subscription_scheduled_change
+from . import subscription_backup
