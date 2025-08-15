2827a6fedf6da90d6a0a04170f24b486aa2ff415
belgianspud <belgianspud@gmail.com>
2025-08-15 18:23:55 -0400

    remove old wizard files from billing module

--- custom_addons/ams_subscription_billing/wizards/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import ams_payment_method_wizard
-from . import ams_billing_run_wizard
-from . import ams_subscription_upgrade_wizard
-from . import ams_extend_grace_period_wizard
-from . import ams_bulk_billing_wizard
-from . import ams_payment_failure_wizard
