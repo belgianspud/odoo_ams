53a8128f9e148c713276c9211dccf6cf2f5af7c0
belgianspud <belgianspud@gmail.com>
2025-08-18 14:15:54 -0400

    start of ams core relationships supporting module.

---
+++ custom_addons/ams_core_relationships/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from . import ams_relationship_type
+from . import ams_partner_relationship
+from . import ams_household
+from . import res_partner
---
+++ custom_addons/ams_core_relationships/wizard/__init__.py
@@ -0,0 +1,4 @@
+# -*- coding: utf-8 -*-
+
+from . import relationship_bulk_import
+from . import household_merge
