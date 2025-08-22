8637e7aebe55ee5f1f2a7cc2b7ebdc71008871a2
belgianspud <belgianspud@gmail.com>
2025-08-22 11:14:43 -0400

    add additional modules that support this ams from odoo store

--- custom_addons/ams_core_audit/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import audit_log
-from . import audit_mixin
-from . import res_partner
--- custom_addons/ams_core_relationships/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import partner_relationship
-from . import relationship_type
-from . import res_partner
